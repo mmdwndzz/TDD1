c089dd9a0a0100cb8c608b4858ac0d5f51ae18a8
dhz <997826541@qq.com>
2020-06-16 17:26:31 +0800

    Functional test now checks we can input a to-do item

--- tdd1.py
+++ tdd1.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -13,22 +15,39 @@ class NewVisitorTest(unittest.TestCase):
 
 
         #she notices the page title and header mention to-do lists
-        self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do',self.broswer.title)
+        header_text=self.broswer.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
 
+
+        #she is invited to enter a to-do item straight away
+        inputbox=self.broswer.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+
+        #she types "buy peacock feathers" into a text box (edith's hobby
+        #is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        #when she hits enter. the pagr updates,and now the page lists
+        #1: buy peacock feathers as an item in a to-do list
+        inputbox.sned_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table=self.broswer.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text=='1:Buy peacock feathers' for row in rows)
+            )
+
+        #there is still atext box inviting her to add another item. she
+        #enters "use peacock feathers to make a fly"(edith is very methodical)
+        self.fail('Finish the test!')
 if __name__=='__main__':
     unittest.main(warnings='ignore')
-#she is invited to enter a to-do item straight away
-
-#she types "buy peacock feathers" into a text box (edith's hobby
-#is tying fly-fishing lures)
-
-#when she hits enter. the pagr updates,and now the page lists
-#1: buy peacock feathers as an item in a to-do list
-
-#there is still atext box inviting her to add another item. she
-#enters "use peacock feathers to make a fly"(edith is very methodical)
-
 #the page updates again,and now shows both items on her list
 #edith wonders whether the site will remember her list then she sees
 #that the site has generated a unique url for her --there is some
